5efa0cdf7efad75d110c7412ae4c31990b06a169
stian <99058760@mydavinci.nl>
2021-11-12 20:28:05 +0100

    changes
    exxpanded the pokemon.cs file with more info

--- Pokemon.cs
+++ Pokemon.cs
@@ -25,5 +25,52 @@ namespace pokeBattle
             Weakness = weaknesses;
             livingPokemon++;
         }
+
+        public void Damage(Pokemon dealer, int attack)
+        {
+            if (Type.TypeWeak == dealer.Type.Type)
+            {
+                Health -= (int)(dealer.Attacks[attack].Damage * weaknesses.Multiplier);
+            } else if(Type.TypeResist == dealer.Type.Type)
+            {
+                Health -= (dealer.Attacks[attack].Damage - Resistances.Resisting);
+            }
+            else
+            {
+                Health -= (dealer.Attacks[attack].Damage);
+            }
+            if (Health <= 0)
+            {
+                livingPokemon--;
+            }
+        }
+
+        public List<Attack> GetAttacks()
+        {
+            return Attacks;
+        }
+
+        public static int GetLivingPokemon()
+        {
+            return livingPokemon;
+        }
+    }
+
+    public class Giratina : Pokemon
+    {
+        public Giratina():base("Giratina", )
+        {
+            Attacks.Add(new Attack("Shadow Force", 120));
+            Attacks.Add(new Attack("Giga Impact", 150));
+        }
+    }
+
+    public class Arceus : Pokemon
+    {
+        public Arceus():base("Arceus", )
+        {
+            Attacks.Add(new Attack("Judgment", 100));
+            Attacks.Add(new Attack("Hyper Beam", 150));
+        }
     }
 }
